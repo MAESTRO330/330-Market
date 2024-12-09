e03294c08b7ab7f8a67116fab310066314837bdc
Artem Deryabin <artem.deryabinn@mail.ru>
2024-12-10 00:21:38 +0300

    Начал работу над моделями

--- Magazine/Magazine/settings.py
+++ Magazine/Magazine/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'market.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
@@ -117,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Here
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- Magazine/market/models.py
+++ Magazine/market/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    sl = 'SL'
+    cm = 'CM'
+
+    role_choices = {
+        sl : 'Продавец',
+        cm : 'Покупатель'
+    }
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=80)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    user_avatar = models.ImageField(upload_to='avatars/', default='avatars\\Login_default_Avatar.png', blank=True, null=True)
+    user_role = models.CharField(choices=role_choices, max_length=3)
+
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    # category = models.CharField()
+    count = models.IntegerField()
+    rating = models.FloatField()
+    seller = models.ForeignKey(User, on_delete=models.SET_NULL)
+    # photos
+    # razmer
+    price = models.FloatField()
